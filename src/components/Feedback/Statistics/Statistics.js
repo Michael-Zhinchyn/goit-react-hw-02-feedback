5142725031dc5fdf27dea783a7d8a07bc049c68a
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-08-07 21:52:18 -0500

    Update Statistics.js
    update task 1 step 2. Added total votes and positive percentage

--- src/components/Feedback/Statistics/Statistics.js
+++ src/components/Feedback/Statistics/Statistics.js
@@ -1,10 +1,16 @@
 export const Statistics = ({ good, neutral, bad }) => {
+  let total = good + neutral + bad;
+  let positivePercent = total !== 0 ? (good / total) * 100 : 0;
+
+  console.log(total);
   return (
     <>
       <h1>Statistics</h1>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Positive: {positivePercent.toFixed(2)}%</p>
     </>
   );
 };
