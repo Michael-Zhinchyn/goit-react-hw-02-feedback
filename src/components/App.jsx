37364c3bcdeac10cda7ef64f349d8b140c3994ae
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-08-07 23:04:27 -0500

    update
    update files and folders structure. added show / hide section feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
-import { Section } from './Section';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
 
 export class App extends Component {
   state = {
@@ -20,26 +21,31 @@ export class App extends Component {
   render() {
     const { good, neutral, bad } = this.state;
     const total = good + neutral + bad;
-    const positivePercentage = total !== 0 ? (good / total) * 100 : 0;
+    const positivePercentage = total === 0 ? 0 : (good / total) * 100;
 
     return (
-      <div>
+      <>
         <Section title="Please leave feedback">
           <FeedbackOptions
             options={['good', 'neutral', 'bad']}
             onLeaveFeedback={this.onLeaveFeedback}
           />
         </Section>
+
         <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={positivePercentage}
-          />
+          {total === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          )}
         </Section>
-      </div>
+      </>
     );
   }
 }
---
+++ src/components/Notification/Notification.js
@@ -0,0 +1,3 @@
+export const Notification = ({ message }) => {
+  return <p>{message}</p>;
+};
