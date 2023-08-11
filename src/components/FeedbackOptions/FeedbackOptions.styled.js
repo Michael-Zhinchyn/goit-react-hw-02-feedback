798d95da3de716418d8b749a923014166f9b7335
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-08-11 13:04:28 -0500

    Update FeedbackOptions.styled.js

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -1,27 +1,61 @@
 import styled from 'styled-components';
 
-const getButtonColor = (option, isHover = false) => {
-  switch (option) {
-    case 'good':
-      return isHover ? 'green' : 'lightgreen';
-    case 'neutral':
-      return isHover ? 'yellow' : 'lightyellow';
-    case 'bad':
-      return isHover ? 'red' : 'lightcoral';
-    default:
-      return isHover ? 'darkgray' : 'gray';
-  }
+const stylesConfig = {
+  good: {
+    default: {
+      background: 'lightgreen',
+      color: 'black',
+      border: '1px solid green',
+    },
+    hover: {
+      background: 'green',
+      color: 'white',
+      border: '1px solid lightgreen',
+    },
+  },
+  neutral: {
+    default: {
+      background: 'lightyellow',
+      color: 'black',
+      border: '1px solid yellow',
+    },
+    hover: {
+      background: 'yellow',
+      color: 'black',
+      border: '1px solid lightyellow',
+    },
+  },
+  bad: {
+    default: {
+      background: 'lightcoral',
+      color: 'black',
+      border: '1px solid red',
+    },
+    hover: {
+      background: 'red',
+      color: 'white',
+      border: '1px solid lightcoral',
+    },
+  },
 };
 
 export const OptionBtn = styled.button`
-  width: 70px;
-  height: 30px;
-  border-radius: 5px;
-  font-size: 15px;
-  background-color: ${props => getButtonColor(props.option)};
-  margin-right: 25px;
+  background-color: ${props =>
+    stylesConfig[props.option]?.default.background || 'gray'};
+
+  color: ${props => stylesConfig[props.option]?.default.color || 'black'};
+  border: ${props =>
+    stylesConfig[props.option]?.default.border || '2px solid gray'};
 
   &:hover {
-    background-color: ${props => getButtonColor(props.option, true)};
+    background-color: ${props =>
+      stylesConfig[props.option]?.hover.background || 'darkgray'};
+    color: ${props => stylesConfig[props.option]?.hover.color || 'white'};
+    border: ${props =>
+      stylesConfig[props.option]?.hover.border || '2px solid gray'};
   }
+
+  margin-right: 20px;
+  width: 75px;
+  height: 30px;
 `;
